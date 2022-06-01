f693d64671e2e39cbe65836e7516eacac269007f
Debby95 <debbyjacksonstar@hotmail.it>
2022-06-01 19:40:37 +0200

    stampa in console l'elenco dei membri del team

---
+++ script.js
@@ -0,0 +1,35 @@
+//stampo in console l'elenco dei membri del team
+const elencoTeamMember = [
+    {
+        nome: "Wayne",
+        cognome: "Barnett",
+        lavoro: "Founder & CEO",
+    }, 
+    {
+        nome: "Angela",
+        cognome: "Caroll",
+        lavoro: "Chief Editor",
+    },
+    {
+        nome: "Walter",
+        cognome: "Gordon",
+        lavoro: "Office Manager",
+    },
+    {
+        nome: "Angela",
+        cognome: "Lopez",
+        lavoro: "Social Media Manager",
+    },
+    {
+        nome: "Scott",
+        cognome: "Estrada",
+        lavoro: "Developer",
+    },
+    {
+        nome: "Barbara",
+        cognome: "Ramos",
+        lavoro: "Graphic Designer",
+    },
+];
+
+console.table(elencoTeamMember);
